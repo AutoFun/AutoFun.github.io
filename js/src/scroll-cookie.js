ec248294a347220a9eb96da5bb6f16a3b565cba4
AutoFun <477955926@qq.com>
2020-04-09 23:26:59 +1000

    Site updated: 2020-04-09 23:26:58

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 09 2020 23:17:54 GMT+1000 (Australian Eastern Standard Time)
+// build time:Thu Apr 09 2020 23:26:56 GMT+1000 (Australian Eastern Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
